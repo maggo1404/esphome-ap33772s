da8c58a56a6001eb609bf6ef23a51e001b4111b4
Marco <509301+maggo1404@users.noreply.github.com>
2026-06-07 22:52:19 +0200

    Update ap33772s.h

--- components/ap33772s/ap33772s.h
+++ components/ap33772s/ap33772s.h
@@ -2,9 +2,15 @@
 #include "esphome/core/component.h"
 #include "esphome/components/output/float_output.h"
 #include "esphome/components/i2c/i2c.h"
+#include <vector>
 
 namespace esphome {
 namespace ap33772s {
+
+struct FixedPdo {
+  uint8_t index;   // 1-basierter PDO-Slot (1..13)
+  uint16_t mv;     // Spannung in mV
+};
 
 class AP33772SOutput : public output::FloatOutput,
                        public Component,
@@ -13,12 +19,16 @@ class AP33772SOutput : public output::FloatOutput,
   void setup() override;
   void dump_config() override;
   void write_state(float state) override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
  protected:
-  uint8_t num_pdos_{0};
-  uint16_t voltages_mv_[7]{};
-  int current_pdo_{-1};
-  void request_pdo_(int pdo_index);
+  std::vector<FixedPdo> pdos_;
+  int current_index_{-1};
+  uint8_t retries_{0};
+
+  void try_read_pdos_();
+  bool read_pdos_();
+  void request_index_(uint8_t pdo_index);
 };
 
 }  // namespace ap33772s
